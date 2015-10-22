ae8779e5f41f6bcc74b4784864e5fe9c49b9fe4e
Chris McLean <havok2905@gmail.com>
2015-10-22 15:56:55 -0400

    make new dirs

--- lib/cs_template/generator.rb
+++ lib/cs_template/generator.rb
@@ -1,11 +1,30 @@
 require 'thor'
+require 'fileutils'
 
 module CsTemplate
   class Generator < Thor
 
     desc 'install', 'Installs the sass files'
-    def install
-      puts 'hi'
+    def generate_sass_files
+      generate_sass_files
+    end
+
+    private
+
+    def seven_one_pattern
+      %w(base components layout pages themes utils vendor)
+    end
+
+    def generate_sass_files
+      seven_one_pattern.each do |dir_name|
+        FileUtils.mkdir_p dir_name
+      end
+    end
+
+    def generate_bourbon
+    end
+
+    def generate_neat
     end
   end
 end
