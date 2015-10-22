b317cc6e22dfe04f2ac9825d1aca75c94e85b2d7
Chris McLean <havok2905@gmail.com>
2015-10-22 16:24:34 -0400

    methods need arguments

--- lib/cs_template/generator.rb
+++ lib/cs_template/generator.rb
@@ -18,7 +18,7 @@ module CsTemplate
 
     def generate_sass_files
       seven_one_pattern.each do |dir_name|
-        copy_directory || init_directory
+        copy_directory(dir_name) || init_directory(dir_name)
       end
     end
 
